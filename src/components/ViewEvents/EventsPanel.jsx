2157f1868528c7c104689d37d8bf39958a5b883c
UniqueCodeMaker <mr.rudraxmehta@gmail.com>
2022-06-20 16:44:45 +0530

    done

--- src/components/ViewEvents/EventsPanel.jsx
+++ src/components/ViewEvents/EventsPanel.jsx
@@ -1,62 +1,62 @@
-// import react , {useState} from "react";
+import react , {useState} from "react";
 
-// import "../../App.css";
-// import Table from 'react-bootstrap/Table';
-// import moment from "moment";
+import "../../App.css";
+import Table from 'react-bootstrap/Table';
+import moment from "moment";
 
 
-// const EventPanel = (props) => {
+const EventPanel = (props) => {
     
-//     return(
-//        <>
-//      {   
+    return(
+       <>
+     {   
 
-//         (moment(props.event.edate).format("YYYY-MM-DD") >= moment(Date.now() ).format('YYYY-MM-DD'))?
-//         <div className="EventsPosts">
+        (moment(props.event.edate).format("YYYY-MM-DD") >= moment(Date.now() ).format('YYYY-MM-DD'))?
+        <div className="EventsPosts">
                  
-//        <img src={props.event.EImageUrl} alt ="event" className="FixImgLen"/>
-//         <div className="EventTimings">
+       <img src={props.event.EImageUrl} alt ="event" className="FixImgLen"/>
+        <div className="EventTimings">
        
-//             <Table bordered    className="Eventtable table bg-dark text-light table-condensed table-responsive overflow-scroll " >
-//             <thead>
-//               <tr >
-//                 <th className="col-sm-1 overflow-scroll h6 small">Lead </th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event Name</th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event Date</th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event Location</th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event Time</th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event Budget</th>
-//                 <th className="col-sm-1 overflow-scroll h6 small">Event fees</th>
-//                 </tr>
-//             </thead>
+            <Table bordered    className="Eventtable table bg-dark text-light table-condensed table-responsive overflow-scroll " >
+            <thead>
+              <tr >
+                <th className="col-sm-1 overflow-scroll h6 small">Lead </th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event Name</th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event Date</th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event Location</th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event Time</th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event Budget</th>
+                <th className="col-sm-1 overflow-scroll h6 small">Event fees</th>
+                </tr>
+            </thead>
 
-//             <tbody>
+            <tbody>
                 
-//                 <td className="col-sm-1 overflow-scroll h6 small">{props.event.lead}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{props.event.event}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{moment(props.event.edate).format('YYYY-MM-DD')}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{props.event.location}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{moment(props.event.etime ,  "HH:mm:ss").format("hh:mm A")}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{props.event.budget}</td>
-//                 <td className="col-sm-1 overflow-scroll h6 small">{props.event.fees}</td> 
+                <td className="col-sm-1 overflow-scroll h6 small">{props.event.lead}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{props.event.event}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{moment(props.event.edate).format('YYYY-MM-DD')}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{props.event.location}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{moment(props.event.etime ,  "HH:mm:ss").format("hh:mm A")}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{props.event.budget}</td>
+                <td className="col-sm-1 overflow-scroll h6 small">{props.event.fees}</td> 
 
-//             </tbody>
-
-
+            </tbody>
 
 
-//             </Table>
-//         </div>
+
+
+            </Table>
+        </div>
         
-//         </div>
-//       :
-//       <></>
-//       }
-//       </>
-//         )
+        </div>
+      :
+      <></>
+      }
+      </>
+        )
 
 
-// }
+}
 
 
-// export default EventPanel;
+export default EventPanel;
